b2d9d23aff6dfb08bea6fa7157e5d6a676c620ae
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2023-05-05 10:17:10 -0700

    implement weather API in app

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useReducer } from 'react';
+import weatherReducer from '../reducers/weather-reducer';
+import { getWeatherFailure, getWeatherSuccess } from '../actions';
 import styled from 'styled-components';
 
 const WeatherWrapper = styled.section`
   border-bottom: 1px solid black;
 `;
 
+const initialState = {
+  isLoaded: false,
+  properties: [],
+  error: null
+};
+
 function Weather () {
+
+  const [state, dispatch] = useReducer(weatherReducer, initialState)
+
+  useEffect(() => {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=bend,or,us&cnt=14&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        } else {
+          return response.json()
+        }
+      })
+      .then((jsonifiedResponse) => {
+        const action = getWeatherSuccess(jsonifiedResponse.list)
+        dispatch(action)
+      })
+      .catch((error) => {
+        const action = getWeatherFailure(error.message)
+        dispatch(action)
+      });
+  }, [])
   return (
     <WeatherWrapper>
       <p>Weather</p>
@@ -14,3 +43,45 @@ function Weather () {
 }
 
 export default Weather;
+
+
+
+
+
+
+
+
+function ShortTermRental () {
+
+
+
+  // const handleAvailabilityData = (availability) => {
+  //   console.log(availability);
+  // };
+
+  const { error, isLoaded, properties } = state;
+
+  if (error) {
+    return ( 
+      <ShortTermRentalWrapper>
+        <h1>Error: {error}</h1>
+      </ShortTermRentalWrapper> 
+    );
+  } else if (!isLoaded) {
+    return (
+      <ShortTermRentalWrapper>
+        <h1>...Loading...</h1>
+      </ShortTermRentalWrapper>
+    );
+  } else {
+    return (
+
+      <ShortTermRentalWrapper>
+        <p>{properties}</p>
+
+      </ShortTermRentalWrapper>
+    );
+  }
+}
+
+export default ShortTermRental;
